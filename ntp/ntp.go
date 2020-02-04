925622ea650bbb28965e560b5ca156921ba601b2
Nacht <realnacht@outlook.com>
2020-02-04 19:13:00 +0800

    Change ntp url to "ntp.cloudcone.com"

--- ntp/ntp.go
+++ ntp/ntp.go
@@ -12,10 +12,7 @@ import (
 const autoUpdatePeriod = time.Minute
 
 var ntpEndpoints = [...]string{
-	"0.pool.ntp.org",
-	"1.pool.ntp.org",
-	"2.pool.ntp.org",
-	"3.pool.ntp.org",
+	"ntp.cloudcone.com",
 }
 
 // Fetch fetches the data on time drift.
